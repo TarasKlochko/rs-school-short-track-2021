d711234975d7a86b7c33c7be7aed9cc7b32605d0
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-04-07 20:17:15 +0300

    feat: do task 04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,89 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = JSON.parse(JSON.stringify(matrix));
+  const lengthM = matrix.length;
+  const lengthI = matrix[0].length;
+
+  for (let i = 0; i < lengthM; i++) {
+    for (let j = 0; j < lengthI; j++) {
+      result[i][j] = 0;
+    }
+  }
+
+  for (let i = 0; i < lengthM; i++) {
+    for (let j = 0; j < lengthI; j++) {
+      if (matrix[i][j]) {
+        if (i === 0 && j === 0) {
+          result[i + 1][j] += 1;
+          result[i][j + 1] += 1;
+          result[i + 1][j + 1] += 1;
+        }
+
+        if (i === 0 && j === lengthI - 1) {
+          result[i][j - 1] += 1;
+          result[i + 1][j] += 1;
+          result[i + 1][j + 1] += 1;
+        }
+
+        if (i === lengthM - 1 && j === lengthI - 1) {
+          result[i - 1][j] += 1;
+          result[i][j - 1] += 1;
+          result[i - 1][j - 1] += 1;
+        }
+
+        if (i === lengthM - 1 && j === 0) {
+          result[i - 1][j] += 1;
+          result[i - 1][j + 1] += 1;
+          result[i][j + 1] += 1;
+        }
+
+        if (i === 0 && j !== 0 && j !== lengthI - 1) {
+          result[i][j - 1] += 1;
+          result[i][j + 1] += 1;
+          result[i + 1][j - 1] += 1;
+          result[i + 1][j + 1] += 1;
+          result[i + 1][j] += 1;
+        }
+
+        if (i !== 0 && i !== lengthM - 1 && j === lengthI - 1) {
+          result[i - 1][j] += 1;
+          result[i + 1][j] += 1;
+          result[i - 1][j - 1] += 1;
+          result[i + 1][j - 1] += 1;
+          result[i][j - 1] += 1;
+        }
+
+        if (i === lengthM - 1 && j !== 0 && j !== lengthI - 1) {
+          result[i][j - 1] += 1;
+          result[i][j + 1] += 1;
+          result[i - 1][j - 1] += 1;
+          result[i - 1][j + 1] += 1;
+          result[i - 1][j] += 1;
+        }
+
+        if (i !== 0 && i !== lengthM - 1 && j === 0) {
+          result[i - 1][j] += 1;
+          result[i + 1][j] += 1;
+          result[i - 1][j - 1] += 1;
+          result[i + 1][j - 1] += 1;
+          result[i][j - 1] += 1;
+        }
+        if (i !== 0 && i !== lengthM - 1 && j !== 0 && j !== lengthI - 1) {
+          result[i - 1][j] += 1;
+          result[i + 1][j] += 1;
+          result[i - 1][j - 1] += 1;
+          result[i + 1][j - 1] += 1;
+          result[i - 1][j + 1] += 1;
+          result[i + 1][j + 1] += 1;
+          result[i][j + 1] += 1;
+          result[i][j - 1] += 1;
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
